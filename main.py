2c9c7c2820895dac2442053ffb413fc849a1448f
jdub4asdfg <junweilee72@gmail.com>
2025-10-02 02:54:22 +0800

    Initialising a database client

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from influxdb_client_3 import InfluxDBClient3
+import os
 
+# Initialising a database client
+token = os.environ.get("DB_TOKEN")
+host = os.environ.get("DB_URL")
+if token is None or host is None:
+    print("database token or host not set in environment variables")
+    os.exit(1)
+
+org = "IOT"
+client = InfluxDBClient3(host=host, token=token, org=org)
+
+# Initialising FastAPI
 api = FastAPI()
 
 
