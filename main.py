5a6a906b4e524013fa44f7e2b5bac3bfda1978d7
jdub4asdfg <junweilee72@gmail.com>
2025-10-10 02:11:05 +0800

    Finished the implementation of endpoint that ingests data from living room sensor

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 from fastapi import FastAPI, Response, status
 from influxdb_client_3 import InfluxDBClient3, Point
 
-from models.models import Kitchen_data, Response_writer
+from models.models import Kitchen_data, Living_room_data, Response_writer
 
 # Initialising a database client
 token = os.environ.get("DB_TOKEN")
@@ -23,7 +23,9 @@ api = FastAPI()
 
 # Endpoint that ingests data from the kitchen
 @api.post("/kitchen_data/", status_code=201)
-async def root(kitchen_data: Kitchen_data, response: Response) -> Response_writer:
+async def post_kitchen_data(
+    kitchen_data: Kitchen_data, response: Response
+) -> Response_writer:
     point = (
         Point("data")
         .tag("location", "kitchen")
@@ -37,8 +39,34 @@ async def root(kitchen_data: Kitchen_data, response: Response) -> Response_write
         fail_msg = f"Unsuccessful write of kitchen data to InfluxDB: {e}"
         print(fail_msg)
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return Response_writer(message = fail_msg)
+        return Response_writer(message=fail_msg)
 
     success_msg = "Successful write of kitchen data to InfluxDB"
     print(success_msg)
-    return Response_writer(message = success_msg)
+    return Response_writer(message=success_msg)
+
+
+# Endpoint that ingests data from the living room environment
+@api.post("/living_room_data", status_code=201)
+async def post_living_room_data(
+    living_room_data: Living_room_data, response: Response
+) -> Response_writer:
+    point = (
+        Point("data")
+        .tag("location", "living room")
+        .field("temperature", living_room_data.temperature)
+        .field("humidity", living_room_data.humidity)
+        .field("illumination", living_room_data.illumination)
+    )
+
+    try:
+        await asyncio.to_thread(client.write, database=database, record=point)
+    except Exception as e:
+        fail_msg = f"Unsuccessful write of living room data to InfluxDB: {e}"
+        print(fail_msg)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return Response_writer(message=fail_msg)
+
+    success_msg = "Successful write of kitchen data to InfluxDB"
+    print(success_msg)
+    return Response_writer(message=success_msg)
--- models/models.py
+++ models/models.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 
+
 class Kitchen_data(BaseModel):
     in_kitchen: bool
     stove_on: bool
+
+
+class Living_room_data(BaseModel):
+    temperature: float
+    humidity: float
+    illumination: int
+
 
 class Response_writer(BaseModel):
     message: str
